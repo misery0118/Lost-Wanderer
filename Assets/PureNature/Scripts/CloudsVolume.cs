b7125ba5e3d6985d461eefa4a620ec29adc91bd3
eli <elikajiwara26@gmail.com>
2024-05-19 12:20:36 +0800

    re import of assets

--- Assets/PureNature/Scripts/CloudsVolume.cs
+++ Assets/PureNature/Scripts/CloudsVolume.cs
@@ -31,13 +31,6 @@ public class CloudsVolume : MonoBehaviour
             matrices[i] = matrix;
             Graphics.DrawMesh(quadMesh, matrix, cloudsMaterial, 0);
         }
-        Graphics.DrawMeshInstanced(quadMesh, 0, cloudsMaterial, matrices, volumeSamples);
+            Graphics.DrawMeshInstanced(quadMesh, 0, cloudsMaterial, matrices, volumeSamples);
     }
-
-    // Method to set the cloud material
-    public void SetCloudMaterial(Material material)
-    {
-        cloudsMaterial = material;
-    }
-    
 }
